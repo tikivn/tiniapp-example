ddba114a3e04b60c6c609ab5ae8e4c0e659784ee
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-07-15 14:12:12 +0700

    fix header

--- custom-header/src/pages/index/index.js
+++ custom-header/src/pages/index/index.js
@@ -4,14 +4,18 @@ Page({
     items: Array.from(Array(100).keys())
   },
   onPageScroll(event) {
-    this.setData({ fixedHeader: event.scrollTop > 147 });
+    this.setData({ fixedHeader: event.scrollTop > 20 });
   },
   onLoad() {
+    // console.log(getCurrentPages()[0].setData({ text: 1 }));
     console.log('this.data.items :>> ', this.data.items);
     my.getSystemInfo({
       success: res => {
         console.log('res :>> ', res);
       }
     })
+  },
+  onTapCart() {
+    console.log('onTapCart');
   }
 });
