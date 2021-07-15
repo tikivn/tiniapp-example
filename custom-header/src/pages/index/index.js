cd4db8f1accab641b506708633147277f4bf03b3
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-07-15 19:47:39 +0700

    clean code

--- custom-header/src/pages/index/index.js
+++ custom-header/src/pages/index/index.js
@@ -6,15 +6,6 @@ Page({
   onPageScroll(event) {
     this.setData({ fixedHeader: event.scrollTop > 20 });
   },
-  onLoad() {
-    // console.log(getCurrentPages()[0].setData({ text: 1 }));
-    console.log('this.data.items :>> ', this.data.items);
-    my.getSystemInfo({
-      success: res => {
-        console.log('res :>> ', res);
-      }
-    })
-  },
   onTapCart() {
     console.log('onTapCart');
   }
